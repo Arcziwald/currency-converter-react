d347d9b4f13503ae48a0f5d19ad1288e1489af2a
Artur Mochnia <amochnia@gmail.com>
2023-06-08 18:54:13 +0100

    added button

--- src/App.js
+++ src/App.js
@@ -5,15 +5,16 @@ import Fieldset from "./Fieldset";
 import Label from "./Label";
 import Input from "./Input";
 import Select from "./Select";
+import Button from "./Button";
 
 function App() {
  return (
     <Container>
-      <div className="element">
+      <Element>
       <Form>
             <Fieldset>
                 <Header title="Przelicznik walut" />
-                   <>
+                  <>
                     <Label text="Waluta">
                       <Select />
                     </Label>
@@ -23,12 +24,11 @@ function App() {
                     <Input />
                     </Label>
                   </>
-                  <button className="form__button">Przelicz</button>
-                  <p><strong><span></span><span></span></strong></p>
+                  <Button text="Przelicz" />
                   <p><a className="form__link" href="https://www.google.pl/search?q=aktualne+kursy+walut&sxsrf=APwXEdchfR1U-81E-QWLdOA1oBqnYxmybw%3A1679774877006&source=hp&ei=nFQfZPKoOqWMlQeDroiQCQ&iflsig=AOEireoAAAAAZB9irQoocohV3Sp8_2uLaCfqQdj4vz21&oq=aktualne+kursy+&gs_lcp=Cgdnd3Mtd2l6EAMYADIECCMQJzIFCAAQgAQyBQgAEIAEMgUIABCABDIICAAQgAQQyQMyCAgAEIoFEJIDMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDoHCAAQigUQQzoRCC4QgAQQsQMQgwEQxwEQ0QM6CwguEIAEELEDEIMBOgsIABCABBCxAxCDAToLCC4QgAQQxwEQrwE6DgguEIoFELEDEIMBENQCOggIABCABBCxAzoLCAAQigUQsQMQgwE6DggAEIAEELEDEIMBEMkDOgoIABCKBRDJAxBDOgsIABCABBCxAxDJAzoICAAQgAQQkgNQAFjqFmCAJWgAcAB4AIABggGIAYwKkgEEMTMuMpgBAKABAQ&sclient=gws-wiz">Sprawdź aktualny kurs </a></p>
             </Fieldset>
         </Form>
-      </div>
+      </Element>
     </Container>   
   );  
 };
---
+++ src/Button/index.js
@@ -0,0 +1,11 @@
+import "./style.css";
+
+const Button = () => (
+    <>
+    <button className="form__button">
+    {text}
+    </button>
+    </>            
+);
+
+export default Button;
