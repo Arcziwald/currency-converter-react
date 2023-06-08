0a0148743e75f9f8b806a9fc389d76cef2b7ccbf
Artur Mochnia <amochnia@gmail.com>
2023-06-08 18:25:48 +0100

    added input

---
+++ src/Input/index.js
@@ -0,0 +1,14 @@
+import "./style.css";
+
+const Input = () => (
+    <input  
+    type="number" 
+    className="form__field" 
+    min="0.01" 
+    step="0.01" a
+    utofocus
+    required
+    />
+);
+
+export default Input;
