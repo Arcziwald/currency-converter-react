48afd3646ef423f16d17dc7743f4924afe36a847
Artur Mochnia <amochnia@gmail.com>
2023-06-09 17:52:51 +0100

    correct mistake

--- src/Input/index.js
+++ src/Input/index.js
@@ -1,12 +1,13 @@
 import "./style.css";
 
 const Input = () => {
+    
     return (
     <input  
     type="number" 
     className="form__field" 
     min="0.01" 
-    step="0.01" a
+    step="0.01"
     autofocus
     required
     placeholder="Wpisz kwotę w zł" 
