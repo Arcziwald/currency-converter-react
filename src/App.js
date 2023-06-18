c0ede190a44d5c1135d2b9576b8e512dfdbff04d
Artur Mochnia <amochnia@gmail.com>
2023-06-18 13:28:10 +0100

    update Result

--- src/App.js
+++ src/App.js
@@ -7,11 +7,10 @@ import Input from "./Input";
 import Select from "./Select";
 import Element from "./Element";
 import Button from "./Button";
+import Result from "./Result";
 import {useState} from "react";
 
-function App() {
-
-  const CurrencyOptions = () => {
+const CurrencyOptions = () => {
     const currencies = [
       { code: 'EUR', rate: 4.55 },
       { code: 'CHF', rate: 4.71 },
@@ -28,12 +27,27 @@ function App() {
     </>
   );
 };
+function App() {
+  const [amount] = useState(0);
+  const [currency] = useState('EUR');
+  const [result] = useState('');
+ 
+  const calculateRate = () => {
+    const currencies = {
+      EUR: 4.55, 
+      CHF: 4.71,
+      GBP: 5.14,
+      USD: 4.32,
+    };
+    return amount / currencies[currency];
+  };
 
- return (
+
+  return (
     <>
     <Container >
       <Element>
-      <Form>
+      <Form onSubmit={onFormSubmit}>
             <Fieldset>
                 <Header title="Przelicznik walut" />
                   <>
@@ -46,7 +60,11 @@ function App() {
                     <Input />
                     </Label>
                   </>
-                  <Button text="Przelicz" />
+                  <Button 
+                  text="Przelicz"
+                  type="submit"
+                  />
+                  <Result result={result} />
                   <p><a className="form__link" href="https://www.google.pl/search?q=aktualne+kursy+walut&sxsrf=APwXEdchfR1U-81E-QWLdOA1oBqnYxmybw%3A1679774877006&source=hp&ei=nFQfZPKoOqWMlQeDroiQCQ&iflsig=AOEireoAAAAAZB9irQoocohV3Sp8_2uLaCfqQdj4vz21&oq=aktualne+kursy+&gs_lcp=Cgdnd3Mtd2l6EAMYADIECCMQJzIFCAAQgAQyBQgAEIAEMgUIABCABDIICAAQgAQQyQMyCAgAEIoFEJIDMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDoHCAAQigUQQzoRCC4QgAQQsQMQgwEQxwEQ0QM6CwguEIAEELEDEIMBOgsIABCABBCxAxCDAToLCC4QgAQQxwEQrwE6DgguEIoFELEDEIMBENQCOggIABCABBCxAzoLCAAQigUQsQMQgwE6DggAEIAEELEDEIMBEMkDOgoIABCKBRDJAxBDOgsIABCABBCxAxDJAzoICAAQgAQQkgNQAFjqFmCAJWgAcAB4AIABggGIAYwKkgEEMTMuMpgBAKABAQ&sclient=gws-wiz">Sprawdź aktualny kurs </a></p>
             </Fieldset>
         </Form>
--- src/Result/index.js
+++ src/Result/index.js
@@ -1,11 +1,6 @@
-const Result = ({ result, amount, selectedCurrency }) => {
-  return (
-    <p>
-      <strong>
-        {amount} PLN = {result.toFixed(2)} {selectedCurrency}
-      </strong>
-    </p>
-  );
+const Result = (rate, amount, currency) => {
+  const result = `${amount.toFixed(2)} PLN = ${rate.toFixed(2)} ${currency}`;
+  setResult(result);
 };
 
 export default Result;
