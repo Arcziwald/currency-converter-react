198021733be6d948985e6f0b742a7da3d906ad33
Artur Mochnia <amochnia@gmail.com>
2023-06-18 20:29:21 +0100

    change function

--- src/Result/index.js
+++ src/Result/index.js
@@ -1,6 +1,10 @@
-const Result = (rate, amount, currency, setResult) => {
-  const result = `${amount.toFixed(2)} PLN = ${rate.toFixed(2)} ${currency}`;
-  setResult(result);
+const Result = ({resultText}) => {
+  return (
+    <div>
+      <h3>Wynik:</h3>
+      <p>{resultText}</p>
+    </div>
+  );
 };
 
 export default Result;
