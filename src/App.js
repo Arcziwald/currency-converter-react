7a3194e5ea79dcede508d851cee9273a4cbd7fe3
Artur Mochnia <amochnia@gmail.com>
2023-06-18 12:53:59 +0100

    added komponent CurrencyOptions

--- src/App.js
+++ src/App.js
@@ -7,8 +7,28 @@ import Input from "./Input";
 import Select from "./Select";
 import Element from "./Element";
 import Button from "./Button";
+import {useState} from "react";
 
 function App() {
+
+  const CurrencyOptions = () => {
+    const currencies = [
+      { code: 'EUR', rate: 4.55 },
+      { code: 'CHF', rate: 4.71 },
+      { code: 'GBP', rate: 5.14 },
+      { code: 'USD', rate: 4.32 },
+  ];
+  return (
+    <>
+      {currencies.map((currency) => (
+        <option key={currency.code} value={currency.code}>
+          {currency.code}
+        </option>
+      ))}
+    </>
+  );
+};
+
  return (
     <>
     <Container >
