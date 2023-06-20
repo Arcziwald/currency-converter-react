44c1b5c37d38a924eaa78428b2f1bb9543b91fb4
Artur Mochnia <amochnia@gmail.com>
2023-06-20 14:00:21 +0100

    formatting

--- src/Result/index.js
+++ src/Result/index.js
@@ -1,17 +1,15 @@
 const Result = ({ resultText }) => (
- 
-    <div>
-      <h3>Result:</h3>
+  <div>
+    <h3>Result:</h3>
     {resultText.setResultText !== undefined && (
       <>
         {resultText.setAmount.toFixed(2)} {resultText.inputCurrency} = {" "}
         <strong>
-          {resultText.setResultText.toFixed(2)} {resultText.outputCurrency}
+        {resultText.setResultText.toFixed(2)} {resultText.outputCurrency}
         </strong>
       </>
     )}
   </div>
-  );
-
+);
 
 export default Result;
