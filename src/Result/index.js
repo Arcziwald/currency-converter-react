183015aa50b49f6919c716e85c85a6ffb6b17177
Artur Mochnia <amochnia@gmail.com>
2023-06-20 13:21:40 +0100

    added update to Result

--- src/Result/index.js
+++ src/Result/index.js
@@ -1,10 +1,17 @@
-const Result = ({ resultText }) => {
-  return (
+const Result = ({ resultText }) => (
+ 
     <div>
-      <h3>Wynik:</h3>
-      <p>{resultText}</p>
-    </div>
+      Result:
+    {resultText.setResultText !== undefined && (
+      <>
+        {resultText.setAmount.toFixed(2)} {resultText.inputCurrency} = {" "}
+        <strong>
+          {resultText.setResultText.toFixed(2)} {resultText.outputCurrency}
+        </strong>
+      </>
+    )}
+  </div>
   );
-};
+
 
 export default Result;
