e7f2063376402debe100ffa236ec754bd61faac5
Artur Mochnia <amochnia@gmail.com>
2023-06-21 12:56:27 +0100

    update result

--- src/Result/index.js
+++ src/Result/index.js
@@ -3,7 +3,7 @@ const Result = ({ result }) => (
     <h3>Result:</h3>
     {result.setResult !== undefined && (
       <>
-        {result.amount.toFixed(2)} {result.inputCurrency} = {" "}
+        {result.amount.toFixed(2)} {result.inputCurrency} &nbsp;PLN&nbsp; = {" "}
         <strong>
         {result.rate.toFixed(2)} {result.outputCurrency}
         </strong>
