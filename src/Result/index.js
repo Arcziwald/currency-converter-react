15179ae06aecc8a1dabf415886976d0e396a0bd4
Artur Mochnia <amochnia@gmail.com>
2023-06-23 15:20:59 +0100

    change export

--- src/Result/index.js
+++ src/Result/index.js
@@ -1,15 +1,11 @@
-const Result = ({ result }) => (
-  
-    result !== undefined && (
-      <>
-        {result.sourceAmount.toFixed(2)}&nbsp;PLN&nbsp;=
-        {" "}
-        <strong>
-          {result.targetAmount.toFixed(2)}&nbsp;{result.currency}
-        </strong>
-      </>
-    )
-  
+export const Result = ({ result }) => (
+!!result && (
+    <>
+      {result.sourceAmount.toFixed(2)}&nbsp;PLN&nbsp;=
+      {" "}
+      <strong>
+        {result.targetAmount.toFixed(2)}&nbsp;{result.currency}
+      </strong>
+    </>
+  )
 );
-
-export default Result;
