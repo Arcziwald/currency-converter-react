fa8ab596303babb48f6b5515cc63970596e0ead1
Artur Mochnia <amochnia@gmail.com>
2023-06-23 13:05:28 +0100

    reconstruction file

--- src/App.js
+++ src/App.js
@@ -1,88 +1,28 @@
 import Form from "./Form";
-import Header from "./Header";
-import Container from "./Container";
-import Fieldset from "./Fieldset";
-import Label from "./Label";
-import Input from "./Input";
-import Select from "./Select";
-import Element from "./Element";
-import Button from "./Button";
 import Result from "./Result";
-import {useState} from "react";
+import { currencies } from "./Form/currencies";
+import "./index.css";
+import { useState } from "react";
 
-const CurrencyOptions = () => {
-    const currencies = [
-      { code: 'EUR', rate: 4.55 },
-      { code: 'CHF', rate: 4.71 },
-      { code: 'GBP', rate: 5.14 },
-      { code: 'USD', rate: 4.32 },
-  ];
+function App() {
+  const [result, setResult] = useState();
+  const calculateRate = (currency, amount) => {
+    const rate = currencies.find(({ code }) => code === currency).rate;
+
+    setResult({
+      sourceAmount: +amount,
+      targetAmount: amount / rate,
+      currency,
+    });
+  };
+  
+
   return (
     <>
-      {currencies.map((currency) => (
-        <option key={currency.code} value={currency.code}>
-          {currency.code}
-        </option>
-      ))}
+      <Form calculateRate={calculateRate} />
+      <Result result={result} />
     </>
   );
-};
-function App() {
-  const [amount, setAmount] = useState(0);
-  const [currency, setCurrency] = useState('EUR');
-  const [result, setResult] = useState('');
- 
-  const calculateRate = (amount, currency) => {
-    const currencies = {
-      EUR: 4.55, 
-      CHF: 4.71,
-      GBP: 5.14,
-      USD: 4.32,
-    };
-    return amount / currencies[currency];
-  };
-
-  const onFormSubmit = (event) => {
-    event.preventDefault();
-    const rate = calculateRate(amount, currency);
-    setResult(rate.toFixed(2));
-  };
-
-  return (
-    <>
-    <Container >
-      <Element>
-      <Form onSubmit={onFormSubmit}>
-            <Fieldset>
-                <Header title="Przelicznik walut" />
-                  <>
-                    <Label text="Waluta">
-                      <Select 
-                      CurrencyOptions={CurrencyOptions} 
-                      value={currency}
-                      onChange={(event) => setCurrency(event.target.value)}
-                      />
-                    </Label>
-                  </>
-                  <>
-                    <Label text="Kwota">
-                    <Input 
-                    value={amount}
-                    onChange= {(event) => setAmount(+event.target.value)}/>
-                    </Label>
-                  </>
-                  <Button 
-                  text="Przelicz"
-                  type="submit"
-                  />
-              </Fieldset>
-      </Form>
-                  <Result result={result} />
-                  <p><a className="form__link" href="https://www.google.pl/search?q=aktualne+kursy+walut&sxsrf=APwXEdchfR1U-81E-QWLdOA1oBqnYxmybw%3A1679774877006&source=hp&ei=nFQfZPKoOqWMlQeDroiQCQ&iflsig=AOEireoAAAAAZB9irQoocohV3Sp8_2uLaCfqQdj4vz21&oq=aktualne+kursy+&gs_lcp=Cgdnd3Mtd2l6EAMYADIECCMQJzIFCAAQgAQyBQgAEIAEMgUIABCABDIICAAQgAQQyQMyCAgAEIoFEJIDMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDoHCAAQigUQQzoRCC4QgAQQsQMQgwEQxwEQ0QM6CwguEIAEELEDEIMBOgsIABCABBCxAxCDAToLCC4QgAQQxwEQrwE6DgguEIoFELEDEIMBENQCOggIABCABBCxAzoLCAAQigUQsQMQgwE6DggAEIAEELEDEIMBEMkDOgoIABCKBRDJAxBDOgsIABCABBCxAxDJAzoICAAQgAQQkgNQAFjqFmCAJWgAcAB4AIABggGIAYwKkgEEMTMuMpgBAKABAQ&sclient=gws-wiz">Sprawdź aktualny kurs </a></p>
-      </Element>
-    </Container>
-    </>
-  );  
-};
+}
 
 export default App;
--- src/Form/index.js
+++ src/Form/index.js
@@ -1,10 +1,64 @@
 import "./form.css";
+import { useState } from "react";
+import { currencies } from "./currencies";
 
-const Form = ({children}) => {
-  return (
-    <form className="form">
-        {children}
-    </form>
+export const Form = ({ calculateRate }) => {
+  const [amount, setAmount] = useState("");
+  const [currency, setCurrency] = useState(currencies[0].code);
+  
+  const onSubmit = (event) => {
+    event.preventDefault();
+    calculateRate(currency, amount);
+  };
+
+    return (
+    <body className="body"> 
+    <div className="container">
+      <div className="element">
+              <form className="form" onSubmit={onSubmit}>
+            <fieldset className="form__fieldset">
+                <legend className="form__legend">Przelicznik walut</legend>
+                   <p>
+                    <label>
+                      <span className="form__labelText"> Waluta </span>
+                      <select className="form__field" 
+                      name="currency"
+                      value={currency}
+                      onChange={({ target }) => setCurrency(target.value)}
+                      >
+                        {currencies.map((currency) => (
+                          <option key={currency.code} value={currency.code}>
+                            {currency.code}
+                          </option>
+                        ))}    
+                      </select>
+                    </label>
+                  </p>
+                  <p>
+                    <label>
+                      <span className="form__labelText"> Kwota </span>
+                     <input
+                      value={amount}
+                      onChange={({ target }) => setAmount(target.value)}
+                      type="number"
+                      className="form__field"
+                      min="0.01"
+                      step="0.01"
+                      autoFocus
+                      required
+                      placeholder="Wpisz kwotę w zł"
+                    />
+                    </label>
+                  </p>
+                  <button className="form__button">Przelicz</button>
+                  <p><strong><span className="js-amount"></span><span className="js-rate"></span></strong></p>
+                  <p><a className="form__link" href="https://www.google.pl/search?q=aktualne+kursy+walut&sxsrf=APwXEdchfR1U-81E-QWLdOA1oBqnYxmybw%3A1679774877006&source=hp&ei=nFQfZPKoOqWMlQeDroiQCQ&iflsig=AOEireoAAAAAZB9irQoocohV3Sp8_2uLaCfqQdj4vz21&oq=aktualne+kursy+&gs_lcp=Cgdnd3Mtd2l6EAMYADIECCMQJzIFCAAQgAQyBQgAEIAEMgUIABCABDIICAAQgAQQyQMyCAgAEIoFEJIDMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDoHCAAQigUQQzoRCC4QgAQQsQMQgwEQxwEQ0QM6CwguEIAEELEDEIMBOgsIABCABBCxAxCDAToLCC4QgAQQxwEQrwE6DgguEIoFELEDEIMBENQCOggIABCABBCxAzoLCAAQigUQsQMQgwE6DggAEIAEELEDEIMBEMkDOgoIABCKBRDJAxBDOgsIABCABBCxAxDJAzoICAAQgAQQkgNQAFjqFmCAJWgAcAB4AIABggGIAYwKkgEEMTMuMpgBAKABAQ&sclient=gws-wiz">Sprawdź aktualny kurs </a></p>
+            </fieldset>
+        </form>
+
+    </div>
+            </div>   
+    </body>
   );
 };
     
