fa839e4e59e5af6f9d4e1d8073e79232939adbf4
Artur Mochnia <amochnia@gmail.com>
2023-06-27 10:26:24 +0100

    moving the component from App to forms

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
 import Form from "./Form";
-
-import { currencies } from "./Form/currencies";
 import "./index.css";
-import { useState } from "react";
 
-function App() {
-  const [result, setResult] = useState();
-  const calculateRate = (currency, amount) => {
-    const rate = currencies.find(({ code }) => code === currency).rate;
-
-    setResult({
-      sourceAmount: +amount,
-      targetAmount: amount / rate,
-      currency,
-    });
-  };
-  
-
+function App({ result, calculateRate }) {
   return (
     <>
       <Form result={result} calculateRate={calculateRate} />
-      
     </>
   );
 }
--- src/Form/index.js
+++ src/Form/index.js
@@ -1,11 +1,22 @@
 import "./form.css";
+import "../index.css";
 import { useState } from "react";
 import { Result } from "../Result";
 import { currencies } from "./currencies";
 
-export const Form = ({ calculateRate, result }) => {
+export const Form = () => {
   const [amount, setAmount] = useState("");
   const [currency, setCurrency] = useState(currencies[0].code);
+  const [result, setResult] = useState();
+  const calculateRate = (currency, amount) => {
+    const rate = currencies.find(({ code }) => code === currency).rate;
+
+    setResult({
+      sourceAmount: +amount,
+      targetAmount: amount / rate,
+      currency,
+    });
+  };
 
   const onSubmit = (event) => {
     event.preventDefault();
@@ -13,59 +24,56 @@ export const Form = ({ calculateRate, result }) => {
   };
 
   return (
-    
-        <form className="form" onSubmit={onSubmit}>
-          <fieldset className="form__fieldset">
-            <legend className="form__legend">Przelicznik walut</legend>
-            <p>
-              <label>
-                <span className="form__labelText"> Waluta </span>
-                <select
-                  className="form__field"
-                  name="currency"
-                  value={currency}
-                  onChange={({ target }) => setCurrency(target.value)}
-                >
-                  {currencies.map((currency) => (
-                    <option key={currency.code} value={currency.code}>
-                      {currency.code}
-                    </option>
-                  ))}
-                </select>
-              </label>
-            </p>
-            <p>
-              <label>
-                <span className="form__labelText"> Kwota </span>
-                <input
-                  value={amount}
-                  onChange={({ target }) => setAmount(target.value)}
-                  type="number"
-                  className="form__field"
-                  min="0.01"
-                  step="0.01"
-                  autoFocus
-                  required
-                  placeholder="Wpisz kwotę w zł"
-                />
-              </label>
-            </p>
-            <button className="form__button">Przelicz</button>
-            <p>
-              <Result result={result} />
-            </p>
-            <p>
-              <a
-                className="form__link"
-                href="https://www.google.pl/search?q=aktualne+kursy+walut&sxsrf=APwXEdchfR1U-81E-QWLdOA1oBqnYxmybw%3A1679774877006&source=hp&ei=nFQfZPKoOqWMlQeDroiQCQ&iflsig=AOEireoAAAAAZB9irQoocohV3Sp8_2uLaCfqQdj4vz21&oq=aktualne+kursy+&gs_lcp=Cgdnd3Mtd2l6EAMYADIECCMQJzIFCAAQgAQyBQgAEIAEMgUIABCABDIICAAQgAQQyQMyCAgAEIoFEJIDMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDoHCAAQigUQQzoRCC4QgAQQsQMQgwEQxwEQ0QM6CwguEIAEELEDEIMBOgsIABCABBCxAxCDAToLCC4QgAQQxwEQrwE6DgguEIoFELEDEIMBENQCOggIABCABBCxAzoLCAAQigUQsQMQgwE6DggAEIAEELEDEIMBEMkDOgoIABCKBRDJAxBDOgsIABCABBCxAxDJAzoICAAQgAQQkgNQAFjqFmCAJWgAcAB4AIABggGIAYwKkgEEMTMuMpgBAKABAQ&sclient=gws-wiz"
-              >
-                Sprawdź aktualny kurs
-              </a>
-            </p>
-          </fieldset>
-        </form>
-   
-   
+    <form className="form container element" onSubmit={onSubmit}>
+      <fieldset className="form__fieldset">
+        <legend className="form__legend">Przelicznik walut</legend>
+        <p>
+          <label>
+            <span className="form__labelText"> Waluta </span>
+            <select
+              className="form__field"
+              name="currency"
+              value={currency}
+              onChange={({ target }) => setCurrency(target.value)}
+            >
+              {currencies.map((currency) => (
+                <option key={currency.code} value={currency.code}>
+                  {currency.code}
+                </option>
+              ))}
+            </select>
+          </label>
+        </p>
+        <p>
+          <label>
+            <span className="form__labelText"> Kwota </span>
+            <input
+              value={amount}
+              onChange={({ target }) => setAmount(target.value)}
+              type="number"
+              className="form__field"
+              min="0.01"
+              step="0.01"
+              autoFocus
+              required
+              placeholder="Wpisz kwotę w zł"
+            />
+          </label>
+        </p>
+        <button className="form__button">Przelicz</button>
+        <p>
+          <Result result={result} />
+        </p>
+        <p>
+          <a
+            className="form__link"
+            href="https://www.google.pl/search?q=aktualne+kursy+walut&sxsrf=APwXEdchfR1U-81E-QWLdOA1oBqnYxmybw%3A1679774877006&source=hp&ei=nFQfZPKoOqWMlQeDroiQCQ&iflsig=AOEireoAAAAAZB9irQoocohV3Sp8_2uLaCfqQdj4vz21&oq=aktualne+kursy+&gs_lcp=Cgdnd3Mtd2l6EAMYADIECCMQJzIFCAAQgAQyBQgAEIAEMgUIABCABDIICAAQgAQQyQMyCAgAEIoFEJIDMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDoHCAAQigUQQzoRCC4QgAQQsQMQgwEQxwEQ0QM6CwguEIAEELEDEIMBOgsIABCABBCxAxCDAToLCC4QgAQQxwEQrwE6DgguEIoFELEDEIMBENQCOggIABCABBCxAzoLCAAQigUQsQMQgwE6DggAEIAEELEDEIMBEMkDOgoIABCKBRDJAxBDOgsIABCABBCxAxDJAzoICAAQgAQQkgNQAFjqFmCAJWgAcAB4AIABggGIAYwKkgEEMTMuMpgBAKABAQ&sclient=gws-wiz"
+          >
+            Sprawdź aktualny kurs
+          </a>
+        </p>
+      </fieldset>
+    </form>
   );
 };
 
