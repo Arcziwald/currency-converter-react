0d43e65a505a204f18975e8d9a29d7ad98c1eaba
Artur Mochnia <amochnia@gmail.com>
2023-07-10 18:00:58 +0100

    added theme.js

---
+++ src/theme.js
@@ -0,0 +1,14 @@
+export const theme = {
+  color: {
+    black: "#000000",
+    white: "#ffffff",
+    silver: "#cccccc",
+    stratos: "#010030",
+    greyNickel: "#b8b8b0",
+    persionGreen: "#009999",
+    bondiBlue: "#00B3B3",
+  },
+  breakpoint: {
+    mobileMax: 768,
+  },
+};
