c960aabdbe6d96790c80a766b281589571025a9d
Artur Mochnia <amochnia@gmail.com>
2023-07-14 17:55:59 +0100

    limited to four currencies

--- src/Form/useRatesData.js
+++ src/Form/useRatesData.js
@@ -2,32 +2,32 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 export const useRatesData = () => {
-    const [ratesData, setRatesData] = useState({
-        status: "loading",
-    });
+  const [ratesData, setRatesData] = useState({
+    status: "loading",
+  });
 
-    useEffect(() => {
-        const currentExchangeRate = "https://api.exchangerate.host/latest?base=PLN";
+  useEffect(() => {
+    const currentExchangeRate = "https://api.exchangerate.host/latest?base=PLN&symbols=USD,EUR,GBP,CHF";
 
-        const axiosRates = async () => {
-            try {
-                const response = await axios.get(currentExchangeRate);
-                const { rates, date } = response.data;
+    const axiosRates = async () => {
+      try {
+        const response = await axios.get(currentExchangeRate);
+        const { rates, date } = response.data;
 
-                setRatesData({
-                    status: "success",
-                    rates,
-                    date,
-                });
-            } catch {
-                setRatesData({
-                    status: "error",
-                });
-            };
-        };
+        setRatesData({
+          status: "success",
+          rates,
+          date,
+        });
+      } catch {
+        setRatesData({
+          status: "error",
+        });
+      }
+    };
 
-        setTimeout(axiosRates, 1000);
-    }, []);
-   
-    return ratesData;    
+    setTimeout(axiosRates, 1500);
+  }, []);
+
+  return ratesData;
 };
