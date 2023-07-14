14fbc0d3d63c0c022dddc08568aafb0374bf648a
Artur Mochnia <amochnia@gmail.com>
2023-07-14 11:52:01 +0100

    change kolor

--- src/theme.js
+++ src/theme.js
@@ -4,7 +4,7 @@ export const theme = {
     white: "#ffffff",
     silver: "#cccccc",
     stratos: "#010030",
-    greyNickel: "#b8b8b0",
+    greyNickel: "#b8b8b065",
     persionGreen: "#009999",
     bondiBlue: "#00B3B3",
     gallery: "#eeeeee",
