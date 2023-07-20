85f7fc900705fb212bd71ab1be5a1d44ed5e40ad
Artur Mochnia <amochnia@gmail.com>
2023-07-20 15:49:28 +0100

    moving the url outside the hook

--- src/Form/useRatesData.js
+++ src/Form/useRatesData.js
@@ -1,13 +1,15 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
+const currentExchangeRate = "https://api.exchangerate.host/latest?base=PLN&symbols=USD,EUR,GBP,CHF&_=" + Date.now();
+
 export const useRatesData = () => {
   const [ratesData, setRatesData] = useState({
     state: "loading",
   });
-
+    
   useEffect(() => {
-    const currentExchangeRate = "https://api.exchangerate.host/latest?base=PLN&symbols=USD,EUR,GBP,CHF&_=" + Date.now();
+  
 
     const axiosRates = async () => {
       try {
